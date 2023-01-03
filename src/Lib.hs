d8726487cfdd86f0596f857833c02eb74fd2cc63
Bjorn Tipling <bjorn.tipling@conductorone.com>
2023-01-03 05:05:16 +0000

    Use a customMain

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,10 +2,35 @@ module Lib
     ( runGame
     ) where
 
-import Brick
+import qualified Brick.Types as T
+import qualified Brick.Main as M
+import qualified Brick.Widgets.Core as W
+import qualified Brick as M
+import qualified Graphics.Vty as V
+import Control.Monad (void)
 
-ui :: Widget ()
-ui = str "Hello, world!"
+drawUI :: St -> [T.Widget Name]
+drawUI s = [W.str $ "Hello, world! Count:" ++ show s]
+
+newtype St = St Int deriving Show
+type Name = ()
+
+handleEvent :: T.BrickEvent Name e -> T.EventM Name St ()
+handleEvent _ = return ()
+
+noopAttr :: M.AttrName
+noopAttr = M.attrName "attr"
+
+app :: M.App St e Name
+app = M.App {M.appDraw = drawUI
+    , M.appChooseCursor = M.neverShowCursor
+    , M.appHandleEvent = handleEvent
+    , M.appAttrMap = const $ M.attrMap  V.defAttr [(noopAttr, M.fg V.cyan)]
+    , M.appStartEvent = return ()
+    }
 
 runGame :: IO ()
-runGame = simpleMain ui
+runGame = do
+    let builder = V.mkVty V.defaultConfig
+    initialVty <- builder
+    void $ M.customMain initialVty builder Nothing app (St 0)
